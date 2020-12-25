b4757f5d9bc8f3f4a9788523c9caec2216352814
YMOUS <59841219+snehccurry@users.noreply.github.com>
2020-12-25 06:39:12 +0000

    Add files via upload

---
+++ data/keycode/keycode.sh
@@ -0,0 +1,28 @@
+#!/bin/
+#            ---------------------------------------------------
+#                             Y - MOUS                                                 
+#            ---------------------------------------------------
+#                Copyright (C) <2020-2021>  <YMOUS>
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [[ "$1" = "" ]]; then
+    echo "Usage: keycode.sh {hidden}"
+    echo
+    echo "Note: This file only for YMOUS, if you execute it without"
+    echo "YMOUS it will not work, this is only a part of main code."
+else
+    {
+    adb shell input keyevent $1
+    } &> /dev/null
+fi
